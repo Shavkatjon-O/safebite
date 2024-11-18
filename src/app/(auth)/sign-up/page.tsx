a833dc9484bb1f99b7bf1747f6c17121be970c8a
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-19 01:53:54 +0500

    feat: sign up step is done

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -115,47 +115,105 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { signUp } from "@/services/auth";
+// import { signUp } from "@/services/auth";
 import { sendVerificationCode } from "@/services/auth";
 import { checkVerificationCode } from "@/services/auth";
 
-const Step1 = ({ onNext }: { onNext: () => void }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setLoading(true);
-    const { success, message } = await signUp(email, password);
-    setLoading(false);
-    if (success) {
-      onNext();  // Move to the next step (Send OTP)
-    } else {
-      setError(message);
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import Link from "next/link";
+import { Loader2 } from "lucide-react";
+
+const Step1FormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+});
+
+const Step1 = ({ onNext }: { onNext: () => void }) => {
+  const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof Step1FormSchema>>({
+    resolver: zodResolver(Step1FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof Step1FormSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      const response = await sendVerificationCode(data.email);
+      setMessage(response.message);
+      if (response.success) {
+        onNext();
+      } else {
+        console.log("Sign-up failed");
+      }
+    } catch (error) {
+      console.log("Error during sign-up:", error);
+      setMessage("An error occurred during sign-up.");
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="email" 
-          placeholder="Email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-        />
-        <input 
-          type="password" 
-          placeholder="Password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-        />
-        <button type="submit" disabled={loading}>Sign Up</button>
-        {error && <p>{error}</p>}
-      </form>
+    <div className="size-full flex justify-center items-center px-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 w-full space-y-3 rounded-md">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Email" {...field}
+                    className="h-14"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {message && (
+            <div className="flex justify-center w-full bg-slate-100 py-2 rounded-md">
+              <span className="text-indigo-500">{message}</span>
+            </div>
+          )}
+          <Button
+            type="submit"
+            className="w-full h-14 bg-custom hover:bg-indigo-800 font-semibold"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <Loader2 className="animate-spin text-white" />
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
+          <div className="w-full flex justify-center">
+            <span className="text-slate-500">Already have account?</span>
+            <Link href="/sign-up" className="text-custom ml-1">Sign In</Link>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
@@ -246,7 +304,7 @@ const Page = () => {
   };
 
   return (
-    <div>
+    <div className="size-full">
       {step === 1 && <Step1 onNext={() => handleNextStep(email)} />}
       {step === 2 && <Step2 email={email} onNext={() => handleNextStep()} />}
       {step === 3 && <Step3 email={email} onNext={() => handleNextStep()} />}
