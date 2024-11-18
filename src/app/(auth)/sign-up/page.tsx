179ca9742f2bd6c28907d1f15df1f90b3262897f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-19 02:10:07 +0500

    feat: otp check

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,134 +1,21 @@
-// "use client";
-
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { useState } from "react";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import { sendVerificationCode } from "@/services/auth";
-// import { z } from "zod";
-// import { useRouter } from "next/navigation";
-// import { Loader2 } from "lucide-react";
-// import Link from "next/link";
-
-// const FormSchema = z.object({
-//   email: z.string().email("Invalid email address"),
-// });
-
-// const Page = () => {
-//   const router = useRouter();
-
-//   const [message, setMessage] = useState("");
-//   const [isSubmitting, setIsSubmitting] = useState(false);
-
-//   const form = useForm<z.infer<typeof FormSchema>>({
-//     resolver: zodResolver(FormSchema),
-//     defaultValues: {
-//       email: "",
-//     },
-//   });
-
-//   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
-//     setIsSubmitting(true);
-//     try {
-//       const response = await sendVerificationCode(data.email);
-
-//       setMessage(response.message);
-
-//       if (response.success) {
-//         console.log("Sign-up successful");
-//         router.push("/verify-otp");
-//       } else {
-//         console.log("Sign-up failed");
-//       }
-//     } catch (error) {
-//       console.log("Error during sign-up:", error);
-
-//       setMessage("An error occurred during sign-up.");
-//     } finally {
-//       setIsSubmitting(false);
-//     }
-//   };
-
-//   return (
-//     <div className="size-full flex justify-center items-center px-4">
-//       <Form {...form}>
-//         <form onSubmit={form.handleSubmit(onSubmit)} className="py-6 max-w-sm w-full px-6 space-y-3 shadow-sm rounded-md border">
-//           <FormField
-//             control={form.control}
-//             name="email"
-//             render={({ field }) => (
-//               <FormItem>
-//                 <FormLabel>Email</FormLabel>
-//                 <FormControl>
-//                   <Input
-//                     type="email"
-//                     placeholder="Email" {...field}
-//                     className="h-12"
-//                   />
-//                 </FormControl>
-//                 <FormMessage />
-//               </FormItem>
-//             )}
-//           />
-
-//           {message && (
-//             <div className="flex justify-center w-full bg-slate-100 py-2 rounded-md">
-//               <span className="text-indigo-500">{message}</span>
-//             </div>
-//           )}
-
-//           <Button
-//             type="submit"
-//             className="w-full h-12 bg-custom hover:bg-indigo-800 font-semibold"
-//             disabled={isSubmitting}
-//           >
-//             {isSubmitting ? (
-//               <Loader2 className="animate-spin text-white" />
-//             ) : (
-//               "Continue"
-//             )}
-//           </Button>
-
-//           <div className="w-full flex justify-center">
-//             <span className="text-slate-500">Already have account?</span>
-//             <Link href="/sign-in" className="text-custom ml-1">Sign In</Link>
-//           </div>
-
-//         </form>
-//       </Form>
-//     </div>
-//   );
-// }
-
-// export default Page;
-
 "use client"
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-// import { signUp } from "@/services/auth";
-import { sendVerificationCode } from "@/services/auth";
-import { checkVerificationCode } from "@/services/auth";
-
-
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
+  FormDescription,
   FormMessage,
 } from "@/components/ui/form";
-
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -136,6 +23,11 @@ import { Button } from "@/components/ui/button";
 import { z } from "zod";
 import Link from "next/link";
 import { Loader2 } from "lucide-react";
+import { 
+  sendVerificationCode,
+  checkVerificationCode,
+} from "@/services/auth";
+
 
 const Step1FormSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -174,7 +66,7 @@ const Step1 = ({ onNext }: { onNext: () => void }) => {
   return (
     <div className="size-full flex justify-center items-center px-4">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 w-full space-y-3 rounded-md">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 w-full space-y-4 rounded-md">
           <FormField
             control={form.control}
             name="email"
@@ -218,27 +110,90 @@ const Step1 = ({ onNext }: { onNext: () => void }) => {
   );
 };
 
-const Step2 = ({ email, onNext }: { email: string, onNext: () => void }) => {
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
 
-  const handleSendCode = async () => {
-    setLoading(true);
-    const response = await sendVerificationCode(email);
-    setLoading(false);
-    setMessage(response.message);
-    if (response.success) {
-      onNext();  // Move to the next step (Verify OTP)
+const Step2FormSchema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters.",
+  }),
+})
+
+const Step2 = ({ email, onNext }: { email: string, onNext: () => void }) => {
+  const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof Step2FormSchema>>({
+    resolver: zodResolver(Step2FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  })
+
+  const onSubmit = async (data: z.infer<typeof Step2FormSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      const response = await checkVerificationCode(email, data.pin);
+      setMessage(response.message);
+      if (response.success) {
+        onNext();
+      } else {
+        console.log("Otp verification failed");
+      }
+    } catch (error) {
+      console.log("Error during sign-up:", error);
+      setMessage("An error occurred during sign-up.");
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
-    <div>
-      <h2>Verify your Email</h2>
-      <button onClick={handleSendCode} disabled={loading}>
-        {loading ? "Sending..." : "Send Verification Code"}
-      </button>
-      {message && <p>{message}</p>}
+    <div className="size-full flex justify-center items-center px-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-4">
+          <FormField
+            control={form.control}
+            name="pin"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>One-Time Password</FormLabel>
+                <FormControl>
+                  <InputOTP maxLength={6} {...field}>
+                    <InputOTPGroup className="w-full flex justify-between">
+                      <InputOTPSlot index={0} className="w-full h-14 text-lg"/>
+                      <InputOTPSlot index={1} className="w-full h-14 text-lg"/>
+                      <InputOTPSlot index={2} className="w-full h-14 text-lg" />
+                      <InputOTPSlot index={3} className="w-full h-14 text-lg" />
+                      <InputOTPSlot index={4} className="w-full h-14 text-lg" />
+                      <InputOTPSlot index={5} className="w-full h-14 text-lg" />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </FormControl>
+                <FormDescription>
+                  Please enter the one-time password sent to your email.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {message && (
+            <div className="flex justify-center w-full bg-slate-100 py-2 rounded-md">
+              <span className="text-indigo-500">{message}</span>
+            </div>
+          )}
+          <Button
+            type="submit"
+            className="w-full h-14 bg-custom hover:bg-indigo-800 font-semibold"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <Loader2 className="animate-spin text-white" />
+            ) : (
+              "Continue"
+            )}
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
